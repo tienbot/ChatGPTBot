c1fa4e1a871eb89c7540a2bdfe8510c49bb0985f
tienbot1 <tien.ru@mail.ru>
2023-05-29 15:38:31 +0900

    add bot code3

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,7 +1,61 @@
-import TeleBot from "telebot"
+// import TeleBot from "telebot"
 
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
+// const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.on("text", msg => msg.reply.text(msg.text))
+// bot.on("text", msg => msg.reply.text(msg.text))
 
-export default bot
+// export default bot
+
+import { Telegraf, session } from 'telegraf'
+import { message } from 'telegraf/filters'
+import { code } from 'telegraf/format'
+import config from 'config'
+import { ogg } from './ogg.js'
+import { openai } from './openai.js'
+import { removeFile } from './utils.js'
+import { initCommand, processTextToChat, INITIAL_SESSION } from './logic.js'
+
+const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
+
+bot.use(session())
+
+bot.command('new', initCommand)
+
+bot.command('start', initCommand)
+
+bot.on(message('voice'), async (ctx) => {
+  ctx.session = ctx.session !== undefined && ctx.session !== null ? ctx.session : INITIAL_SESSION;
+
+  try {
+    await ctx.reply(code('Сообщение принял. Жду ответ от сервера...'))
+    const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+    const userId = String(ctx.message.from.id)
+    const oggPath = await ogg.create(link.href, userId)
+    const mp3Path = await ogg.toMp3(oggPath, userId)
+
+    removeFile(oggPath)
+
+    const text = await openai.transcription(mp3Path)
+    await ctx.reply(code(`Ваш запрос: ${text}`))
+
+    await processTextToChat(ctx, text)
+  } catch (e) {
+    console.log(`Error while voice message`, e.message)
+  }
+})
+
+bot.on(message('text'), async (ctx) => {
+  ctx.session = ctx.session !== undefined && ctx.session !== null ? ctx.session : INITIAL_SESSION;
+
+  try {
+    await ctx.reply(code('Сообщение принял. Жду ответ от сервера...'))
+    await processTextToChat(ctx, ctx.message.text)
+  } catch (e) {
+    console.log(`Error while voice message`, e.message)
+  }
+})
+
+bot.launch()
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
